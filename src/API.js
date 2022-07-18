b6ff6e19a1f3f383ad0ae9063078453aaf8c637d
OkelloAndrewPeters <okelloan@gmail.com>
2022-07-18 11:11:01 +0300

    intial commit

---
+++ src/API.js
@@ -0,0 +1,15 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '9c3f54c3b4mshb9ac923dac73078p1f3c4ejsn115c42421d5b',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "60efc3c096fad486e6dab0fcd2e30212"; 
+
+
